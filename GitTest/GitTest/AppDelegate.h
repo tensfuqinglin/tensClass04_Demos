3a493781208c1be203f003c59c9f81a2c44854a0
tens_fuqinglin <tens_fuqinglin@163.com>
2016-08-22 13:24:19 +0800

    create project

---
+++ GitTest/GitTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitTest
+//
+//  Created by tens04 on 16/8/22.
+//  Copyright © 2016年 tens+十安科技. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
